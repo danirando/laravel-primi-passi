620c34515a13c4233befb98eb43ce81f2987fca9
danirando <danielerando@tiscali.it>
2025-08-01 18:04:36 +0200

    add about and contacts pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,11 @@
     </head>
     <body>
         <h1>Hello World!</h1>
+
+        <ul>
+            <li><a href="{{ route('about') }}">about us</a></li>
+            <li><a href="{{ route('contacts') }}">conctats</a></li>
+        </ul>
     </body>
     </html>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about', fn () => view('about'))->name('about');
+Route::get('/contacts', fn () => view('contacts'))->name('contacts');
